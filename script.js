09258e4ded596d4670405096aa10774b923b72c1
StGeorgeAinShams <abanoubadel118@gmail.com>
2026-03-02 23:48:11 +0200

    Implement counting feature in script.js
    Add counting functionality to display goals.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let num = document.querySelectorAll(".number");
+let section = document.querySelector(".stats .box");
 
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++
+    }, 100);
+}
+
+startCount(num[0]);
