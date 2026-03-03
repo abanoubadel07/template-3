de341dd9ab101cecb0f766b1b23c2dcb21e1a4b8
StGeorgeAinShams <abanoubadel118@gmail.com>
2026-03-03 20:58:01 +0200

    Update number counting logic and selectors

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-let nums = document.querySelectorAll(".number");
-let section = document.querySelector(".stats .box");
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false;
+
+window.onscroll = function () {
+    if (window.scrollY >= statsSection.offsetTop) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
 
 function startCount(el) {
     let goal = el.dataset.goal;
     let count = setInterval(() => {
-        el.textContent++
-    }, 100);
-}
-
-startCount(document.querySelectorAll(".number")[0]);
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
+};
